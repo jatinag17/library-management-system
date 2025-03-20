d2cf3f3ecb1e722f8ad95ffdffa3ef9ce1dbea73
jatinag17 <jatinagrawal128@gmail.com>
2025-03-20 22:22:11 +0530

    Add header file with Bootstrap and custom styles for dashboard layout

---
+++ LMS/include/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script src="./assets/js/7d9b19adfc.js" crossorigin="anonymous"></script>
+    <title>Dashboard | Library</title>
+</head>
+
+<body>
