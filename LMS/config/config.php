a2b83b2ff2133b4ebd5b611dffca63255c6aab02
jatinag17 <jatinagrawal128@gmail.com>
2025-03-21 01:16:27 +0530

    Add environment-specific configuration for local and production setups

--- LMS/config/config.php
+++ LMS/config/config.php
@@ -1,4 +1,19 @@
 <?php
+session_start();
+if ($_SERVER['HTTP_HOST'] == 'localhost') {
+    define("BASE_URL", "http://localhost/Library-Management-system/LMS/");
+    define("DIR_URL", $_SERVER['DOCUMENT_ROOT'] . "/Library-Management-system/LMS/");
 
-define("BASE_URL", "http://localhost/Library-Management-system/LMS/");
-define("DIR_URL",$_SERVER['DOCUMENT_ROOT'] . "/Library-Management-system/LMS/");
+    define("SERVER_NAME", "localhost");
+    define("USERNAME", "root");
+    define("PASSWORD", "");
+    define("DATABASE", "lms");
+} else {
+    define("BASE_URL", "https://LMS.com");
+    define("DIR_URL", $_SERVER['DOCUMENT_ROOT']);
+
+    define("SERVER_NAME", "");
+    define("USERNAME", "");
+    define("PASSWORD", "");
+    define("DATABASE", "");
+}
