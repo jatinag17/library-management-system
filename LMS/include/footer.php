e669b0815943e17d3edad20681e4c5960a0618ed
jatinag17 <jatinagrawal128@gmail.com>
2025-03-22 17:25:56 +0530

    Add DataTables integration to footer.php for enhanced table functionality

--- LMS/include/footer.php
+++ LMS/include/footer.php
@@ -1,8 +1,15 @@
+ <script src="<?php echo BASE_URL ?>assets/js/jquery-3.5.1.js"></script>
+ <script src="<?php echo BASE_URL ?>assets/js/jquery.dataTables.min.js"></script>
+ <script src="<?php echo BASE_URL ?>assets/js/dataTables.bootstrap5.min.js"></script>
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
      integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
      crossorigin="anonymous"></script>
 
-
+ <script>
+     $(document).ready(function() {
+         $('#data-table').DataTable();
+     });
+ </script>
  </body>
 
  </html>
