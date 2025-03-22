1dd7ad9160f7a17febf423deca8e53863b249151
jatinag17 <jatinagrawal128@gmail.com>
2025-03-22 17:25:46 +0530

    Refactor sidebar.php for improved structure and readability

--- LMS/include/sidebar.php
+++ LMS/include/sidebar.php
@@ -1,116 +1,129 @@
-<!--Offcanvas start-->
-
-
+<!--Offcanvas Menu start-->
 <div class="offcanvas offcanvas-start bg-dark text-white sidebar-nav" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
-
     <div class="offcanvas-body">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <div class="text-secondary small text-uppercase fw-bold">CORE</div>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#"><i class="fa-solid fa-gauge me-2"></i>Dashboard</a>
-            </li>
-            <li class="nav-item my-0">
-                <hr />
-            </li>
-            <li class="nav-item">
-                <div class="text-secondary small text-uppercase fw-bold">INVENTORY</div>
-            </li>
+        <nav class="navbar-dark">
+            <ul class="navbar-nav">
+                <li>
+                    <div class="text-secondary small fw-bold text-uppercase">
+                        Core
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="<?php echo BASE_URL ?>dashboard">
+                        <i class="fas fa-tachometer-alt-fast me-2"></i> Dashboard</a>
+                </li>
+                <li class="my-0">
+                    <hr />
+                </li>
 
-            <li class="nav-item">
-                <a class="nav-link sidebar-link" data-bs-toggle="collapse" href="#booksMgmt" role="button" aria-expanded="false"
-                    aria-controls="booksMgmt">
-                    <i class="fa-solid fa-book-open me-2"></i> Books Management
-                    <span class="right-icon float-end"><i class="fa-solid fa-chevron-down "></i></span>
-                </a>
-                <div class="collapse" id="booksMgmt">
-                    <div>
-                        <ul class="navbar-nav ps-3">
-                            <li>
-                                <a href="<?php echo BASE_URL ?>books/add.php" class="nav-link"><i class="far fa-plus me-2"></i>Add New</a>
-                            </li>
-                            <li>
-                                <a href="<?php echo BASE_URL ?>books" class="nav-link"><i class="fa-solid fa-bars me-2"></i>Manage All</a>
-                            </li>
-                        </ul>
+                <li>
+                    <div class="text-secondary small fw-bold text-uppercase">
+                        Inventory
                     </div>
-                </div>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link sidebar-link" data-bs-toggle="collapse" href="#studentMgmt" role="button" aria-expanded="false"
-                    aria-controls="studentMgmt">
-                    <i class="fa-solid fa-users me-2"></i>Students Management
-                    <span class="right-icon float-end"><i class="fa-solid fa-chevron-down "></i></span>
-                </a>
-                <div class="collapse" id="studentMgmt">
-                    <div>
-                        <ul class="navbar-nav ps-3">
-                            <li>
-                                <a href="./add-students.php" class="nav-link"><i class="far fa-plus me-2"></i>Add New</a>
-                            </li>
-                            <li>
-                                <a href="./manage-students.php" class="nav-link"><i class="fa-solid fa-bars me-2"></i>Manage All</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </li>
-            <li class="nav-item my-0">
-                <hr />
-            </li>
-            <li class="nav-item">
-                <div class="text-secondary small text-uppercase fw-bold">BUSINESS</div>
-            </li>
+                </li>
 
-            <li class="nav-item">
-                <a class="nav-link sidebar-link" data-bs-toggle="collapse" href="#booksLoanMgmt" role="button" aria-expanded="false"
-                    aria-controls="booksLoanMgmt">
-                    <i class="fa-solid fa-book-open-reader me-2"></i>Books Loan
-                    <span class="right-icon float-end"><i class="fa-solid fa-chevron-down "></i></span>
-                </a>
-                <div class="collapse" id="booksLoanMgmt">
-                    <div>
-                        <ul class="navbar-nav ps-3">
-                            <li>
-                                <a href="./add-loan.php" class="nav-link"><i class="far fa-plus me-2"></i>Add New</a>
-                            </li>
-                            <li>
-                                <a href="./manage-loans.php" class="nav-link"><i class="fa-solid fa-bars me-2"></i>Manage All</a>
-                            </li>
-                        </ul>
+                <li class="nav-item">
+                    <a class="nav-link sidebar-link" data-bs-toggle="collapse" href="#booksManagement" role="button" aria-expanded="false" aria-controls="booksManagement">
+                        <i class="fa-solid fa-book me-2"></i>
+                        Books Management
+                        <span class="right-icon float-end"><i class="fas fa-chevron-down"></i></span>
+                    </a>
+
+                    <div class="collapse" id="booksManagement">
+                        <div>
+                            <ul class="navbar-nav ps-3">
+                                <li>
+                                    <a href="<?php echo BASE_URL ?>books/add" class="nav-link"><i class="fa-solid fa-plus me-2"></i> Add New</a>
+                                </li>
+                                <li>
+                                    <a href="<?php echo BASE_URL ?>books/" class="nav-link"><i class="fa-solid fa-list me-2"></i> Manage All</a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link sidebar-link" data-bs-toggle="collapse" href="#subsMgmt" role="button" aria-expanded="false"
-                    aria-controls="subsMgmt">
-                    <i class="fa-solid fa-indian-rupee-sign me-2"></i>Subscription
-                    <span class="right-icon float-end"><i class="fa-solid fa-chevron-down "></i></span>
-                </a>
-                <div class="collapse" id="subsMgmt">
-                    <div>
-                        <ul class="navbar-nav ps-3">
-                            <li>
-                                <a href="./add-plans.php" class="nav-link"><i class="far fa-plus me-2"></i>Plans</a>
-                            </li>
-                            <li>
-                                <a href="./purchase-history.php" class="nav-link"><i class="fa-solid fa-bars me-2"></i>Purchase History</a>
-                            </li>
-                        </ul>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link sidebar-link" data-bs-toggle="collapse" href="#studentsManagement" role="button" aria-expanded="false" aria-controls="studentsManagement">
+                        <i class="fa-solid fa-users me-2"></i>
+                        Students
+                        <span class="right-icon float-end"><i class="fas fa-chevron-down"></i></span>
+                    </a>
+
+                    <div class="collapse" id="studentsManagement">
+                        <div>
+                            <ul class="navbar-nav ps-3">
+                                <li>
+                                    <a href="<?php echo BASE_URL ?>students/add.php" class="nav-link"><i class="fa-solid fa-plus me-2"></i> Add New</a>
+                                </li>
+                                <li>
+                                    <a href="<?php echo BASE_URL ?>students" class="nav-link"><i class="fa-solid fa-list me-2"></i> Manage All</a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
-            </li>
-            <li class="nav-item my-0">
-                <hr />
-            </li>
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#"><i class="fa-solid fa-arrow-right-from-bracket me-2"></i>Logout</a>
-            </li>
-        </ul>
-        </ul>
+                </li>
+                <li class="my-0">
+                    <hr />
+                </li>
+
+                <li>
+                    <div class="text-secondary small fw-bold text-uppercase">
+                        Business
+                    </div>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link sidebar-link" data-bs-toggle="collapse" href="#loanManagement" role="button" aria-expanded="false" aria-controls="loanManagement">
+                        <i class="fas fa-book-reader me-2"></i>
+                        Books Loan
+                        <span class="right-icon float-end"><i class="fas fa-chevron-down"></i></span>
+                    </a>
+
+                    <div class="collapse" id="loanManagement">
+                        <div>
+                            <ul class="navbar-nav ps-3">
+                                <li>
+                                    <a href="<?php echo BASE_URL ?>loans/add.php" class="nav-link"><i class="fa-solid fa-plus me-2"></i> Add New</a>
+                                </li>
+                                <li>
+                                    <a href="<?php echo BASE_URL ?>loans" class="nav-link"><i class="fa-solid fa-list me-2"></i> Manage All</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link sidebar-link" data-bs-toggle="collapse" href="#subscriptionManagement" role="button" aria-expanded="false" aria-controls="subscriptionManagement">
+                        <i class="fa-solid fa-indian-rupee-sign me-2"></i>
+                        Subscription
+                        <span class="right-icon float-end"><i class="fas fa-chevron-down"></i></span>
+                    </a>
+
+                    <div class="collapse" id="subscriptionManagement">
+                        <div>
+                            <ul class="navbar-nav ps-3">
+                                <li>
+                                    <a href="<?php echo BASE_URL ?>subscriptions" class="nav-link"><i class="fa-solid fa-plus me-2"></i> Plans</a>
+                                </li>
+                                <li>
+                                    <a href="<?php echo BASE_URL ?>subscriptions/purchase-history.php" class="nav-link"><i class="fa-solid fa-list me-2"></i> Purchase
+                                        History</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </li>
+                <li class="my-0">
+                    <hr />
+                </li>
+
+                <li class="nav-item">
+                    <a href="<?php echo BASE_URL ?>logout.php" class="nav-link"><i class="fa-solid fa-right-from-bracket me-2"></i> Logout</a>
+                </li>
+            </ul>
+        </nav>
     </div>
 </div>
-
-
-<!--Offcanvas  end-->
+<!--Offcanvas Menu end-->
