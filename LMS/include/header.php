c8a852769d645e90c6cb3bd20027d97732ae0032
jatinag17 <jatinagrawal128@gmail.com>
2025-03-22 17:24:23 +0530

    Fix formatting in header.php for improved readability

--- LMS/include/header.php
+++ LMS/include/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="<?php echo BASE_URL?>assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="<?php echo BASE_URL ?>assets/css/bootstrap.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="<?php echo BASE_URL ?>assets/css/style.css">
     <script src="<?php echo BASE_URL ?>assets/js/7d9b19adfc.js" crossorigin="anonymous"></script>
     <title>Dashboard | Library</title>
